82593f7a81899d459b5170fc556d6c48bdd389ca
himesha <himesha.patel7@gmail.com>
2025-04-17 12:39:03 +1000

    nextjs error issue

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      'react': path.resolve(__dirname, './node_modules/react'),
-    },
-  },
-  root: './',
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'styled-components', 'three', '@react-three/fiber', '@react-three/drei']
-  },
   build: {
-    commonjsOptions: {
-      include: [/three/, /@react-three/, /maath/, /node_modules/],
-    },
+    outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
@@ -30,5 +18,6 @@ export default defineConfig({
     }
   }
 });
+
 
 
